81561cb3bab9cd1aa8cc23eb0570b9fa036374de
EC2 Default User <ec2-user@ip-172-31-2-67.us-east-2.compute.internal>
2025-05-19 00:58:15 +0000

    Added state update functionality

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ def handle_confirm(data):
     print('Received confirmation:', data)
     emit('server_response', {'message': 'Hello from Flask!'}, broadcast=True)
 
+@socketio.on('update_state')
+def update_state(data):
+    cards = [
+        {"owner": "ALICE", "index": 0, "value": "5 / 7"}, 
+        {"owner": "BOB", "index": 0, "value": "42 / 24"}, 
+        {"owner": "BOB", "index": 1, "value": "123 / 321"}, 
+    ]
+    emit('set_state', cards)
+
+
+
 if __name__ == '__main__':
     print("Running on port 5000")
     socketio.run(app, host='0.0.0.0', port=5000)
--- static/main.js
+++ static/main.js
@@ -1,10 +1,9 @@
 let NAME_SELF = "BOB";
 let NAME_OPPONENT = "ALICE";
+const socket = io();
 
 // temp for testing
 document.addEventListener("DOMContentLoaded", () => {
-  const socket = io();
-
   document.getElementById("confirmBtn").onclick = function () {
     socket.emit("confirm", { user: "Bob", selected: [42, 93] });
   };
@@ -14,19 +13,20 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function select_card(index, player_id) {
-  let owner_name = get_owner(player_id);
-  console.log(owner_name, "'s card selected:", index);
-}
 
+
+
+// card access /////////////////////////////////////////////////////////
 function get_card_id(owner,index) {
   return { owner, index };
 }
 
 function get_card_obj(card_id) {
   const buttons = document.querySelectorAll('.textbox');
-  var index = card_id.get("index");
-  var owner = card_id.get("owner");
+  
+
+  var index = card_id.index;
+  var owner = card_id.owner;
 
   if(owner==NAME_SELF) {
     index += 10;
@@ -34,6 +34,9 @@ function get_card_obj(card_id) {
   return buttons[index];
 }
 
+
+
+// card data access /////////////////////////////////////////////////////
 function get_card_value(card_id) {
   var card_obj = get_card_obj(card_id);
   return card_obj.textContent;
@@ -44,12 +47,32 @@ function set_card_value(card_id,value) {
   card_obj.textContent = value;
 }
 
+// card selection ///////////////////////////////////////////////////////
 function get_owner(player_id) {
   if(player_id==0) {
     return NAME_OPPONENT;
   }
   return NAME_SELF;
 }
+
+function select_card(index, player_id) {
+  let owner_name = get_owner(player_id);
+  console.log(owner_name, "'s card selected:", index);
+
+  socket.emit("update_state", {}); 
+}
+
+
+// update game state
+socket.on('set_state', (cards) => {
+  console.log("Received cards:", cards);
+
+  cards.forEach(card => {
+    let card_id = card;
+    set_card_value(card_id,card.value);
+  });
+});
+
 
 
 
