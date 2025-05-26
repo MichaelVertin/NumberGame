b5ae1d0712d24d872d1aa0c34be05d6981675c08
MichaelVertin <mikevertin64@gmail.com>
2025-05-26 22:12:31 +0000

    Created Functional Template for Lobby

--- static/lobby.js
+++ static/lobby.js
@@ -1,9 +1,22 @@
 const socket = io();
+let USERNAME = "";
 
 
-async function create_player() {
-  const username = document.getElementById("self-name").value;
 
+document.addEventListener("DOMContentLoaded", () => {
+  USERNAME = prompt("Enter Your Name");
+  const name_field = document.getElementById("name-field");
+  name_field.innerHTML = USERNAME;
+
+  const name_field2 = document.getElementById("your-name");
+  name_field2.value = USERNAME;
+  name_field2.disabled = true;
+
+  create_player(USERNAME);
+});
+
+
+async function create_player(username) {
   const response = await fetch("/set_username", {
     method: "POST",
     headers: {
@@ -13,17 +26,18 @@ async function create_player() {
   });
 }
 
-function access_game() {
-  const game_name = document.getElementById("game-name").value;
-  socket.emit("join_game", {"game_name": game_name});
-}
-
-function create_game() {
-  const game_name = document.getElementById("game-name").value;
+function submit_new_game() {
+  const game_name = document.getElementById("new-game-name").value;
   const opponent_name = document.getElementById("opponent-name").value;
 
   socket.emit("create_game", {"opponent_name": opponent_name, 
                               "game_name": game_name}); 
+}
+
+function submit_existing_game() {
+  const game_name = document.getElementById("filter-existing-game").value;
+  
+  socket.emit("join_game", {"game_name": game_name});
 }
 
 socket.on("load_game", () => {
